de96c5ff6898dfb635aca876dfe3e93f1754ea73
alex <akorotkov@mcfr.ru>
2015-09-24 19:33:35 +0600

    created updated

--- units/models.py
+++ units/models.py
@@ -18,22 +18,22 @@ def get_item_types(lang="en"):
         return tuple(map(lambda x: tuple(x[2:]), ITEM_TYPES))
     return tuple(map(lambda x: tuple(x[:2]), ITEM_TYPES))
 
-class Created():
-    created_at = models.ForeignKey(User)
-    created_by = models.DateTimeField(auto_now_add=True)
-        
-class Updated():
-    updated_at = models.ForeignKey(User)
-    updated_by = models.DateTimeField(auto_now=True)
-
 class Unit(models.Model, Created, Updated):
     name = models.CharField(max_length=256, blank=False, null=False)
     desc = models.TextField()
+    created_at = models.ForeignKey(User)
+    created_by = models.DateTimeField(auto_now_add=True)
+    updated_at = models.ForeignKey(User)
+    updated_by = models.DateTimeField(auto_now=True)
 
 class Item(models.Model, Created, Updated):
     content = models.TextField(null=False, blank=False)
     order = models.IntegerField(default=0, null=False, blank=False)
     item_type = models.CharField(max_length=3, choices=get_item_types(), null=False, blank=False)
     unit = models.ForeignKey(Unit, null=False, blank=False)
+    created_at = models.ForeignKey(User)
+    created_by = models.DateTimeField(auto_now_add=True)
+    updated_at = models.ForeignKey(User)
+    updated_by = models.DateTimeField(auto_now=True)
 
 
