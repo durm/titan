036421715b0faf99f037b8ba0fc1b96b1ba68b85
alex <akorotkov@mcfr.ru>
2015-09-24 20:21:11 +0600

    header

--- units/urls.py
+++ units/urls.py
@@ -2,9 +2,9 @@ from django.conf.urls import patterns, url
 from units.views import *
 
 urlpatterns = patterns('',
-    url(r'^save/$', save),
-    url(r'^save/unit(?P<num>[0-9]+)/$', save),
-    url(r'^unit(?P<num>[0-9]+)/$', page),
-    url(r'^mylist/$', mylist),
+    url(r'^save/$', save, name="save"),
+    url(r'^save/unit(?P<num>[0-9]+)/$', save, name="save_unit"),
+    url(r'^unit(?P<num>[0-9]+)/$', page, name="page"),
+    url(r'^mylist/$', mylist, name="mylist"),
 )
 
