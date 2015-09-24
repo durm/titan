baf85b9a8bf5f6af4767b83cee604597d5bc97f4
alex <akorotkov@mcfr.ru>
2015-09-24 17:54:31 +0600

    register

--- titan/views.py
+++ titan/views.py
@@ -3,6 +3,7 @@ from django.core.urlresolvers import reverse
 from titan import settings
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
+from django.template.context_processors import csrf
 
 def index(request):
     return redirect(settings.LOGIN_URL)
@@ -15,4 +16,6 @@ def register(request):
             return HttpResponseRedirect("/me/")
     else:
         form = UserCreationForm()
-    return render_to_response("registration/register.html", {'form': form})
+    c = {'form': form}
+    c.update(csrf(request))
+    return render_to_response("registration/register.html", c)
