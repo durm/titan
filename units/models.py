9c4f036db1c0f2913298bcc17c6163dcd8d08319
alex <akorotkov@mcfr.ru>
2015-09-24 18:54:58 +0600

    models

--- units/models.py
+++ units/models.py
@@ -17,11 +17,25 @@ def get_item_types(lang="en"):
         return tuple(map(lambda x: tuple(x[2:]), ITEM_TYPES))
     return tuple(map(lambda x: tuple(x[:2]), ITEM_TYPES))
 
-class Unit(models.Model):
+class Created(models.Model):
+    created_at = models.ForeignKey(User)
+    created_by = models.DateTimeField(auto_now_add=True)
+    
+    class Meta:
+        abstract = True
+        
+class Updated(models.Model):
+    updated_at = models.ForeignKey(User)
+    updated_by = models.DateTimeField(auto_now=True)
+    
+    class Meta:
+        abstract = True
+
+class Unit(models.Model, Created, Updated):
     name = models.CharField(max_length=256, blank=False, null=False)
     desc = models.TextField()
 
-class Item(models.Model):
+class Item(models.Model, Created, Updated):
     content = models.TextField(null=False, blank=False)
     order = models.IntegerField(default=0, null=False, blank=False)
     item_type = models.CharField(max_length=3, choices=get_item_types(), null=False, blank=False)
