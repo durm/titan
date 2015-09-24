ac64a312cf30bab84456b9106cd3f3435e96518e
alex <akorotkov@mcfr.ru>
2015-09-24 20:37:03 +0600

    index

--- titan/urls.py
+++ titan/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
-from titan.views import register
+from titan.views import *
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
